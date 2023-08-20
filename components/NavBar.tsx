20efb45ca461a3742237b09524a82b179bef3c74
sagnik-05 <98590203+sagnik-05@users.noreply.github.com>
2023-08-20 19:31:03 +0530

    initiated

---
+++ components/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
+function NavBar() {
+  return (
+    <div className="flex justify-between items-center p-3 px-5 shadow-sm border-b-[1px]">
+      <Image src='/demo_logo.PNG' alt='logo' width={70} height={60} />
+      <div className=" hidden md:flex gap-5">
+        <h2 className=" hover:bg-blue-400 hover:cursor-pointer px-3 p-2 rounded-full hover:text-white font-semibold">Home</h2>
+        <h2 className=" hover:bg-blue-400 hover:cursor-pointer px-3 p-2 rounded-full hover:text-white font-semibold">History</h2>
+        <h2 className=" hover:bg-blue-400 hover:cursor-pointer px-3 p-2 rounded-full hover:text-white font-semibold">Contact Us</h2>
+      </div>
+      <UserButton/>
+    </div>
+  );
+}
+
+export default NavBar;
