50e9c9f1f8034ca6d5cb6723162d3623e201b05f
aman08-cool <aman.12100190@lpu.in>
2023-08-24 17:30:41 +0530

    Added Some more features

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CarsFilterOption from "@/components/Home/CarsFilterOption";
 import Hero from "@/components/Home/Hero";
 import SearchInput from "@/components/Home/SearchInput";
 import Image from "next/image";
@@ -7,7 +8,8 @@ export default function Home() {
   return (
     <div className="p-5 sm:px-10 md:py-20">
       <Hero />
-      <SearchInput/>
+      <SearchInput />
+      <CarsFilterOption />
     </div>
   );
 }
